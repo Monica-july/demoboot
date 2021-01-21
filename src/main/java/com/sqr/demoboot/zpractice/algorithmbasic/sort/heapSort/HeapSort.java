842089c877e5ce95dfa38477c369a6993c9bf08f
july <1753846238@qq.com>
2021-01-21 22:53:10 +0800

    设计模式--观察者模式提交1 堆排序修改

--- src/main/java/com/sqr/demoboot/zpractice/algorithmbasic/sort/heapSort/HeapSort.java
+++ src/main/java/com/sqr/demoboot/zpractice/algorithmbasic/sort/heapSort/HeapSort.java
@@ -5,16 +5,46 @@ import java.util.Arrays;
 public class HeapSort {
 
     public static void main(String[] args) {
-        int[] nums = {1,2,3,4,5};
-        heapSort(nums);
+        int[] nums = {1,2,3,4,5,6,7,8,9,10,11,12};
+//        heapSort1(nums);
+        heapSort2(nums);
         System.out.println(Arrays.toString(nums));
     }
-    private static void heapSort(int[] nums){
+
+    /**
+     * 0-n 个元素组成大根堆 从0到n依次添加到堆中 O(nlogn)
+     * pop最大值 将0位置元素与heapSize-1位置元素交换，整理出新的大根堆 此时最大元素已排好序
+     * 循环第二部，每次heapSize-- 直到heapSize==0 结束  排序完成
+     * @param nums
+     */
+    private static void heapSort1(int[] nums){
         if (nums.length<=1){
             return;
         }
         for (int i = 0; i < nums.length ; i++) {
             heapInsert(nums,i);
+        }
+        int heapSize = nums.length;
+        swap(nums,0,--heapSize);
+        while (heapSize>0){
+            heapify(nums,0,heapSize);
+            swap(nums,0,--heapSize);
+        }
+    }
+
+    /**
+     * 组织大根堆步骤优化
+     * heapSort1 是从0开始逐个添加
+     * heapSort2 将原数组想象为一个完全二叉树
+     * 从最后一个元素向上看，heapify 组织大根堆
+     * @param nums
+     */
+    private static void heapSort2(int[] nums){
+        if (nums.length<=1){
+            return;
+        }
+        for (int i=nums.length-1 ; i>=0 ; i--){
+            heapify(nums,i,nums.length);
         }
         int heapSize = nums.length;
         swap(nums,0,--heapSize);
